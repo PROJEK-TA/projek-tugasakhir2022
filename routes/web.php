d8689c466fde94eba1cb73d0cd4397ca234527c2
gitakartika25 <teresagita25@gmail.com>
2022-03-14 23:37:16 +0700

    add controller and model barang

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/user', function () {
     return view('users.index');
 });
+
+Route::resource('barang',BarangController::class);
