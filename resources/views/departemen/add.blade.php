7ac93caf548c0f0610addf18dc297e238f24f1e6
Evita Putri Nauli <eputrinauli@gmail.com>
2022-04-04 13:53:27 +0700

    add halaman tambah departemen

--- resources/views/departemen/add.blade.php
+++ resources/views/departemen/add.blade.php
@@ -12,12 +12,12 @@
             <div class="card-body">
                 <form>
                     <div class="form-group">
-                        <label class="form-label" for="email">Kode Departemen</label>
-                        <input type="email" class="form-control" id="email1">
+                        <label class="form-label" for="kd_departemen">Kode Departemen</label>
+                        <input type="text" class="form-control" id="kd_departemen1">
                     </div>
                     <div class="form-group">
-                        <label class="form-label" for="pwd">Nama Departemen</label>
-                        <input type="password" class="form-control" id="pwd">
+                        <label class="form-label" for="nm_departemen">Nama Departemen</label>
+                        <input type="text" class="form-control" id="nm_departemen1">
                     </div><br>
                     <button type="submit" class="btn btn-primary">Tambahkan</button>
                 </form>
