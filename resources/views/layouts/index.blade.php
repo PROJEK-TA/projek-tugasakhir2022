46f587db04b2b4376706abab62cdf059023e8580
gitakartika25 <teresagita25@gmail.com>
2022-05-24 00:26:07 +0700

    Add navigation sidebar barang

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -290,6 +290,34 @@
                                 </a>
                             </li>
                             <li class="nav-item">
+                                <a class="nav-link " href="/statuspeminjamanbarang">
+                                    <i class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                            fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> K </i>
+                                    <span class="item-name">Status Peminjaman</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="/userpeminjamanbarang">
+                                    <i class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                            fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> K </i>
+                                    <span class="item-name">Ajukan Peminjaman</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link " href="/peminjamanbarang">
                                     <i class="icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
