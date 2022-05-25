08c7efdceec8b1dcda1e90a4708a940fbae72c72
Evita Putri Nauli <eputrinauli@gmail.com>
2022-05-25 10:17:37 +0700

    Add UI statuspeminjamanruangan requestor

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -440,6 +440,34 @@
                                     <span class="item-name">Peminjaman</span>
                                 </a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="/statuspeminjamanruangan">
+                                    <i class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                            fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> K </i>
+                                    <span class="item-name">Status Peminjaman</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="/userpeminjamanruangan">
+                                    <i class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                            fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> K </i>
+                                    <span class="item-name">Ajukan Peminjaman</span>
+                                </a>
+                            </li>
                         </ul>
                     </li>
                     <li class="nav-item">
