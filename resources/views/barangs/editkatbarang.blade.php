429f906be36bef21c66a7e27fb7fd6394e859c1a
gitakartika25 <teresagita25@gmail.com>
2022-05-27 20:21:20 +0700

    fixing edit kategori barang

--- resources/views/barangs/editkatbarang.blade.php
+++ resources/views/barangs/editkatbarang.blade.php
@@ -10,11 +10,12 @@
                </div>
             </div>
             <div class="card-body">
-                <form action="{{route('kategoribarang.store')}}" method="POST">
+                <form action="{{ url('kategoribarang/'.$katbar->id)}}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    <input type="hidden" name="_method" value="PATCH">
                     <div class="form-group">
                         <label class="form-label" for="nm_kategori"><b>Nama Kategori</b></label>
-                        <input type="text" class="form-control" id="nm_kategori" name="kategoribarang" placeholder="Input nama kategori...">
+                        <input type="text" class="form-control" id="nm_kategori" name="kategoribarang" value="{{ $katbar->nama_kategbarang }}" placeholder="Input nama kategori...">
                     </div><br><br>
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
