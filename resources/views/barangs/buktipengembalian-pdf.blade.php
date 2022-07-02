a0a149d3b429e2002ff73288d7c976dc09bd5262
gitakartika25 <teresagita25@gmail.com>
2022-07-02 22:42:02 +0700

    edit laporan bukti pengembalian aset

--- resources/views/barangs/buktipengembalian-pdf.blade.php
+++ resources/views/barangs/buktipengembalian-pdf.blade.php
@@ -6,12 +6,12 @@
     <style type="text/css">
     table {
         border-style: double;
-        border-width: 6px;
+        border-width: 8px;
         border-color: white;
     }
 
     table tr .text2 {
-        text-align: right;
+        text-align: left;
         font-size: 15px;
     }
 
@@ -28,68 +28,73 @@
 
 <body>
     <center>
-        <h4>Laporan Bukti Pengembalian Aset Barang</h1>
+    <!-- <img src="{{ asset('imageuser/Universal PT Tempu Rejo.PNG') }}" alt="" style="width: 35px; border-radius:100px;" > -->
+        <h3>Laporan Bukti Pengembalian Aset Barang</h1>
     </center>
     <center>
         <table>
-            <tr class="text2">
-                <td>Nama Peminjam</td>
-                <td width="800">: {{ $pengembalianbarang->nama_peminjam }}</td>
+            <tr >
+                <td width="200">Nama Peminjam</td>
+                <td >: {{ $pengembalianbarang->nama_peminjam }}</td>
             </tr>
             <tr>
                 <td>Kode Peminjaman</td>
-                <td width="600">: {{ $pengembalianbarang->kode_peminjaman }}</td>
+                <td >: {{ $pengembalianbarang->kode_peminjaman }}</td>
             </tr>
             <tr>
                 <td>Nama Barang</td>
-                <td width="600">: {{ $pengembalianbarang->barang->kode_barang }} -
+                <td >: {{ $pengembalianbarang->barang->kode_barang }} -
                     {{ $pengembalianbarang->barang->nama_barang }} ({{ $pengembalianbarang->merk->nama_merkbarang }})
                 </td>
             </tr>
             <tr>
                 <td>Lokasi Barang</td>
-                <td width="600">: {{$pengembalianbarang->lokasi->nama_lokasibarang}}
+                <td >: {{$pengembalianbarang->lokasi->nama_lokasibarang}}
                     ({{$pengembalianbarang->gudang->nama_gedung}})</td>
             </tr>
             <tr>
                 <td>Milik</td>
-                <td width="600">: {{$pengembalianbarang->departemen->nama_departemen}}</td>
+                <td >: {{$pengembalianbarang->departemen->nama_departemen}}</td>
             </tr>
             <tr>
                 <td>Keperluan</td>
-                <td width="600">: {{$pengembalianbarang->deskripsi}}</td>
+                <td >: {{$pengembalianbarang->deskripsi}}</td>
             </tr>
             <tr>
                 <td>Petugas Penanggung Jawab Aset IT</td>
-                <td width="600">: {{ Auth::user()->name }}</td>
+                <td >: {{ Auth::user()->name }}</td>
             </tr>
             <tr>
                 <td>Tanggal Pinjam</td>
-                <td width="600">: {{$pengembalianbarang->tanggal_pinjam}}</td>
+                <td >: {{$pengembalianbarang->tanggal_pinjam}}</td>
             </tr>
             <tr>
                 <td>Jatuh Tempo</td>
-                <td width="600">: {{$pengembalianbarang->tanggal_kembali}}</td>
+                <td >: {{$pengembalianbarang->tanggal_kembali}}</td>
+            </tr>
+            <tr>
+                <td>Tanggal Pengembalian</td>
+                <td >: {{$pengembalianbarang->tanggal_pengembalian}}</td>
             </tr>
             <tr>
                 <td>Kondisi Barang Setelah Dipinjam</td>
-                <td width="600">: {{ $pengembalianbarang->kondisi_setelahdipinjam }}</td>
+                <td >: {{ $pengembalianbarang->kondisi_setelahdipinjam }}</td>
             </tr>
             <tr>
                 <td>Catatan</td>
-                <td width="600">: {{ $pengembalianbarang->catatan }}</td>
+                <td >: {{ $pengembalianbarang->catatan }}</td>
             </tr>
             <!-- <tr>
                 <td>Bukti Pengembalian</td>
-                <td width="600">: {{ $pengembalianbarang->bukti_pengembalian }}</td>
+                <td >: {{ $pengembalianbarang->bukti_pengembalian }}</td>
             </tr> -->
         </table>
         <br>
 
         <table width="500">
             <tr>
-                <td width="430"><br><br></td>
-                <td class="text" align="center">Peminjam<br><br><br><br>(...........................)</td>
+                <td width="400"><br><br></td>
+                <td class="text" align="center">Peminjam<br><br><br><br><br>(...................................)</td>
             </tr>
         </table>
     </center>
