643faf89aed82a8bfe1b6e18a2d828913ecf27eb
gitakartika25 <teresagita25@gmail.com>
2022-07-11 19:13:28 +0700

    finish

--- resources/views/ruangan/editgedung.blade.php
+++ resources/views/ruangan/editgedung.blade.php
@@ -6,7 +6,7 @@
     <div class="card">
         <div class="card-header d-flex justify-content-between">
             <div class="header-title">
-                <h4 class="card-title">Edit Gedung</h4>
+                <h4 class="card-title">Edit Gudang</h4>
             </div>
         </div>
         <div class="card-body">
